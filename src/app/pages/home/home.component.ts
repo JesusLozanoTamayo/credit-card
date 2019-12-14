d7f8408dae7768202d31a5dd429ab4ca3e596c09
jesus lozano <jalozanot@gmail.com>
2019-12-14 07:39:31 -0500

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { ClienteService } from '../../services/cliente.service';
+import { stringify } from 'querystring';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor( private auth: AuthService,
+               private router: Router, private clienteService: ClienteService ) { }
+
+  ngOnInit() {
+    this.getClientId();
+  }
+
+  salir() {
+
+    this.auth.logout();
+    this.router.navigateByUrl('/login');
+
+  }
+
+
+  public getClientId() {
+
+    this.clienteService.getClientId('1').subscribe((res: any) => {
+        console.log(JSON.stringify(res))
+    });
+
+  }
+
+
+}
---
+++ src/app/services/cliente.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor(private http: HttpClient) {
+    console.log('Spotify Service Listo');
+  }
+
+  public getClientId(id: string) {
+    return this.http.get('http://localhost:8080/cliente/buscar/2').pipe(
+      map(data => data)
+    );
+  }
+
+
+}
